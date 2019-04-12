d07d6b00f7ecb9936026772e858f95602d90b226
Teodor <ttoshkov@asteasolutions.com>
2019-04-12 19:21:28 +0300

    Add comments for MyString

--- exercises/08 - Exam Prep/MyString.h
+++ exercises/08 - Exam Prep/MyString.h
@@ -2,11 +2,16 @@
 #include <string>
 #include <iostream>
 
+/*
+    Barebones string implementation. Only to be used during exams when std::string is forbidden and we don't have time to implement a better one
+*/
+
 class MyString
 {
  public:
     MyString()
     {
+        // Bad. Small allocation in default constructor
         m_str = new char[1];
         m_str[0] = '\0';
     }
@@ -21,8 +26,9 @@ class MyString
         del();
     }
 
-    MyString& operator=(const MyString& other)
+    MyString& operator=(const MyString& other)  // No operator=(const char*) will make calls such as MyString x; x = "kappa"; very inefficient
     {
+        // Bad. See Ex03
         if (this != &other)
         {
             del();
@@ -37,6 +43,7 @@ class MyString
         copy(other.m_str);
     }
 
+    // If you need to match strings
     bool operator==(const MyString& other) const
     {
         int len = strlen(m_str) + 1;
@@ -48,6 +55,7 @@ class MyString
         return !(*this == other);
     }
 
+    // If you need to order strings lexicographically
     bool operator<(const MyString& other) const
     {
         int len = strlen(m_str) + 1;
@@ -59,17 +67,20 @@ class MyString
         std::cout << m_str << std::endl;
     }
 
+    // If you need to output a string
     friend std::ostream& operator<<(std::ostream& out, const MyString& str)
     {
         return out << str.m_str;
     }
 
+    // Fuck it, just give me the cstring
     const char* c_str() const
     {
         return m_str;
     }
 
  private:
+    // No size and capacity => doesn't support concatenation well, and .size() will be very slow
     char* m_str = nullptr;
 
     void del()
